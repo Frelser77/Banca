21a1cc11fef488c50a94d53510e88fe5438edcf6
D'Amico Salvatore Alessandro <122369512+Frelser77@users.noreply.github.com>
2024-01-31 16:20:22 +0100

    Aggiungere i file di progetto.

---
+++ Banca/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Banca
+{
+    internal class ContoCorrente
+    {
+        // PROPRIETÁ
+        public string NumeroConto { get; set; }
+        public string Intestatario { get; set; }
+        public DateTime AperturaConto { get; set; }
+        public int SaldoConto { get; set; }
+
+        //COSTRUTTORE
+        public ContoCorrente(string numeroConto, string intestatario, DateTime aperturaConto, int saldo)
+        {
+            NumeroConto = numeroConto;
+            Intestatario = intestatario;
+            AperturaConto = aperturaConto;
+            SaldoConto = saldo;
+        }
+        /*    public ContoCorrente(decimal conto)
+            {
+                SaldoConto = conto;
+            }*/
+
+        //METODI
+        public string OpenCC()
+        {
+            return ($"Numero dei ContoCorrente {NumeroConto} " +
+                    $"\nintestato al Sg.{Intestatario} " +
+                    $"in data {AperturaConto} " +
+                    $"\ncon saldo al momento dell'apertura {SaldoConto}");
+        }
+
+        public void PrelievoCC(int importo)
+        {
+            if (importo <= SaldoConto && importo > 0)
+            {
+                SaldoConto -= importo;
+            }
+            else
+            {
+                Console.WriteLine("Importo non valido o saldo insufficiente");
+            }
+        }
+
+        public bool PrimoDepositoCC(int importo)
+        {
+            if (importo >= 1000)
+            {
+                SaldoConto = importo;
+                return true;
+            }
+            else
+            {
+                SaldoConto = 0;
+                Console.WriteLine("L'importo del primo deposito deve essere almeno di 1000€ \no momentaneamente fuori servizio");
+                return false;
+            }
+        }
+
+        public void DepositoCC(int importo)
+        {
+            if (importo > 0)
+            {
+                SaldoConto += importo;
+            }
+            else
+            {
+                Console.WriteLine("L'importo del deposito non é corretto \no momentaneamente fuori servizio");
+            }
+        }
+
+        public string GetFullInfo()
+        {
+            return $"Info principali del conto: " +
+                   $"\nNumero CC: {NumeroConto}" +
+                   $"\nIntestato a: {Intestatario}" +
+                   $"\nin data: {AperturaConto}" +
+                   $"\nIStantanea saldo contabile: {SaldoConto}";
+        }
+
+    }
+}
---
+++ Banca/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Banca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContoCorrente conto1 = null;
+            bool contoRegistrato = false;
+            int saldo = 0;
+
+            Console.WriteLine("Benvenuto nella nostra banca vuoi aprire un conto?(y/n)");
+            string aprireConto = Console.ReadLine();
+            if (aprireConto == "y")
+            {
+                // RACCOLTA DATI DA IMPUT
+                Console.WriteLine("Iniziamo");
+                Console.WriteLine("Inserisci i tuoi dati per completare la procedura");
+                Console.WriteLine("Inserisci nome e cognome");
+                string intestatario = Console.ReadLine();
+                Console.WriteLine("\n");
+
+                Console.WriteLine("Inserisci il numero del conto:");
+                string numeroConto = Console.ReadLine();
+                Console.WriteLine("\n");
+
+                DateTime aperturaConto = DateTime.Now;
+
+                // COSTRUTTORE CLASSE CONTOCORRENTE
+                conto1 = new ContoCorrente(intestatario, numeroConto, aperturaConto, 0);
+                contoRegistrato = true;
+
+                while (true)
+                {
+                    Console.WriteLine("Inserisci quanto vuoi depositare (min 1000€):");
+                    saldo = Convert.ToInt32(Console.ReadLine());
+
+                    if (saldo >= 1000)
+                    {
+                        conto1.PrimoDepositoCC(saldo);
+                        break; // Esce dal ciclo se il deposito é valido
+                    }
+                    else
+                    {
+                        Console.WriteLine("L'importo del primo deposito deve essere almeno di 1000€.");
+                        // Il ciclo continua 
+                    }
+                }
+
+                // Dopo il while
+                conto1.OpenCC();
+                Console.WriteLine(conto1.GetFullInfo());
+
+                while (contoRegistrato)
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Desideri fare altre operazioni?(y/n)");
+                    string start = Console.ReadLine();
+                    if (start == "y")
+                    {
+                        Console.WriteLine("\n");
+
+                        Console.WriteLine("Cosa vuoi fare?");
+                        Console.WriteLine("\n");
+                        Console.WriteLine("\n1)Deposito" +
+                                          "\n2)Prelievo");
+                        string scelta = Console.ReadLine();
+
+                        if (scelta == "1")
+                        {
+                            Console.WriteLine("Quanto vuoi depositare?");
+                            saldo = Convert.ToInt32(Console.ReadLine());
+                            conto1.DepositoCC(saldo);
+                            Console.WriteLine("\n");
+                            Console.WriteLine($"Il tuo saldo corrente é {saldo}");
+                            Console.WriteLine(conto1.GetFullInfo());
+
+                        }
+                        else if (scelta == "2")
+                        {
+                            Console.WriteLine("Quanto vuoi prelevare?pasa");
+                            saldo = Convert.ToInt32(Console.ReadLine());
+                            conto1.PrelievoCC(saldo);
+                            Console.WriteLine($"Il tuo saldo corrente é {saldo}");
+                            Console.WriteLine(conto1.GetFullInfo());
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Scelta non valida");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Grazie e arrivederci");
+                        break; // uscita ciclo se imput diverso da y
+                    }
+
+
+                    Console.WriteLine("Grazie e arrivederci!");
+
+                    Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
